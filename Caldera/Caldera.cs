f14441be556d8991761dac9fcdb355ca712ec9e6
kratox <rcmartinez281@gmail.com>
2022-10-25 13:30:06 +0200

    push

---
+++ Caldera/Caldera.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caldera
+{
+
+    internal class Caldera
+    {
+        private int temperatura;
+        private Estados estado;
+
+        public event EventHandler<CalderaArgumentosEvento> CambioTemperatura;
+        public event EventHandler<CalderaArgumentosEvento> CambioEstado;
+        public event EventHandler StopCaldera;
+    
+        public int Temperatura
+        {
+            get { return temperatura; }
+            set { temperatura = value; }
+        }
+
+        public Estados Estado
+        {
+            get { return estado; }
+            set { estado = value; }
+        }
+
+    }
+
+    public class CalderaArgumentosEvento
+    {
+        private int temperatura;
+        private Estados estado;
+        CalderaArgumentosEvento(int Grados,DateTime fecha,Estados estado)
+        {
+
+        }
+        public int Temperatura
+        {
+            get { return temperatura; }
+            set { temperatura = value; }
+        }
+
+    }
+    internal enum Estados : int
+    {
+        Correcto = 0, //(verde)
+        Alerta = 1, //     (amarillo)
+        Peligro = 2//(rojo)
+    }
+
+}
+
