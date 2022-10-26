de52ce2212b4e4b80233b9930a9bcca7c1dbfbcb
kratox <rcmartinez281@gmail.com>
2022-10-26 13:46:56 +0200

    push

---
+++ TiendaDeMascotas/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TiendaDeMascotas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+        List<Mascota> mascotas = new List<Mascota>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            aniadirAnimales();
+            cargarLista();
+        }
+        private void cargarLista()
+        {
+            lbAnimal.Items.Clear();
+            foreach (Mascota mas in mascotas)
+            {
+                lbAnimal.Items.Add(mas);
+            }
+
+        }
+        private void aniadirAnimales()
+        {
+            mascotas.Add(new Mascota("rober", "gato", "paco"));
+            mascotas.Add(new Mascota("juan", "perro", "Rex"));
+            mascotas.Add(new Mascota("javi", "gato", "fufi"));
+            mascotas.Add(new Mascota("concha", "gato", "cuki"));
+            mascotas.Add(new Mascota("adam", "perro", "perro"));
+
+
+
+        }
+        private void lbAnimal_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Mascota temp = lbAnimal.SelectedItem as Mascota;
+            if (temp != null)
+            {
+                nomPropi.Text = temp.Propietario;
+                nomEspecie.Text = temp.Especie;
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Mascota ms = (Mascota)lbAnimal.SelectedItem;
+            mascotas.Remove(ms);
+            nomPropi.Text = "";
+            nomEspecie.Text = "";
+            cargarLista();
+        }
+    }
+}
---
+++ TiendaDeMascotas/Mascota.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiendaDeMascotas
+{
+    
+    internal class Mascota
+    {
+        private String nombre;
+
+        public String Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        private String propietario;
+
+        public String Propietario
+        {
+            get { return propietario; }
+            set { propietario = value; }
+        }
+        private String especie;
+        private string v1;
+        private string v2;
+
+        public Mascota(string v1, string v2,string nomb)
+        {
+            this.propietario = v1;
+            this.especie = v2;
+            this.nombre = nomb;
+        }
+
+        public String Especie
+        {
+            get { return especie; }
+            set { especie = value; }
+        }
+        public override string ToString()
+        {
+            return Nombre;
+        }
+
+
+
+    }
+	
+
+}
