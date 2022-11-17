ff15223282e19605329a5dfebf4be7f17b9fe8e6
kratox <rcmartinez281@gmail.com>
2022-11-17 09:49:15 +0100

    17/11/2022

---
+++ WpfAppDockPanel/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace WpfAppDockPanel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            dispatcherTimer.Tick += DispatcherTimer_Tick; ;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+        }
+
+        private void DispatcherTimer_Tick(object? sender, EventArgs e)
+        {
+            labelReloj.Content = DateTime.Now.ToString("T");
+        }
+
+    }
+}
